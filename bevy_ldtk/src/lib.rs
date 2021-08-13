73690239ff9f580d6dca6a720c3209d915bbe5ab
Zachary Bush <zach@zmbush.com>
2021-08-12 18:18:27 -0700

    Import ldtk and bevy_ldtk crates (#1)
    * Import ldtk and bevy_ldtk crates
    
    * Rustfmt config
    
    * Add deny

---
+++ bevy_ldtk/src/lib.rs
@@ -0,0 +1,35 @@
+use bevy::prelude::*;
+
+mod assets;
+mod render;
+
+pub use assets::LdtkProject;
+pub use render::{EntitySpawn, LdtkProjectBundle, LdtkProjectCfg, LdtkRenderType};
+
+#[derive(Debug)]
+pub struct LdtkPlugin;
+
+pub static LDTK_RENDER: &str = "ldtk_render";
+pub static LDTK_CLEANUP: &str = "ldtk_cleanup";
+pub static LDTK_HOT_RELOAD: &str = "ldtk_hot_reload";
+
+impl Plugin for LdtkPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.add_asset::<assets::LdtkProject>()
+            .init_asset_loader::<assets::LdtkProjectLoader>()
+            .add_event::<render::EntitySpawn>()
+            .add_system(render::render_ldtk_projects.system().label(LDTK_RENDER))
+            .add_system(
+                render::ldtk_entity_cleanup
+                    .system()
+                    .label(LDTK_CLEANUP)
+                    .before(LDTK_RENDER),
+            )
+            .add_system(
+                render::ldtk_hot_reload
+                    .system()
+                    .label(LDTK_HOT_RELOAD)
+                    .before(LDTK_CLEANUP),
+            );
+    }
+}
